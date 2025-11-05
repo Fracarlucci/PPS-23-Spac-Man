662d60a53c0a8b24d856a7a3d4a15326827cc418
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-11-05 15:41:11 +0100

    feat: add position and direction

---
+++ src/main/scala/model/Direction.scala
@@ -0,0 +1,12 @@
+package model
+
+sealed trait Direction
+object Direction:
+    case object Up extends Direction
+    case object Down extends Direction
+    case object Left extends Direction
+    case object Right extends Direction
+
+
+
+
--- src/test/scala/SimpleTest.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-
-class SimpleTest extends AnyFlatSpec:
-    // a simple test only for testing github action
-    it should "be true" in:
-        val x = 2
-        assert(x == 2)
