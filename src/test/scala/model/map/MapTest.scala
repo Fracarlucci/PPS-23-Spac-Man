3d7bb6e2972bc4b1d6c6f34b6a6bca6a3de16fa3
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-11-09 20:44:24 +0100

    refactor: refactor MapTest

--- src/test/scala/model/map/MapTest.scala
+++ src/test/scala/model/map/MapTest.scala
@@ -4,10 +4,13 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 import model.MapDSL
 import model.Wall
-import model.PacMan
-import model.Ghost
 import model.Position2D
 import model.board
+import org.scalactic.anyvals.PosInt
+import model.Position2DTest
+import model.SpacManBasic
+import model.GhostBasic
+import model.Direction
 
 class MapTest extends AnyFlatSpec with Matchers:
 
@@ -19,14 +22,17 @@ class MapTest extends AnyFlatSpec with Matchers:
 
     "DSL" should "create the entity" in:
         val dsl = MapDSL(board(5, 5))
+        val wall = Wall(Position2D(2, 1))
+        val pacMan = SpacManBasic(Position2D(3, 1), Direction.Right, 0)
+        val ghost = GhostBasic(Position2D(4, 1), Direction.Right, 1.0, 1)
 
         import dsl.*
 
-        place a Wall at position (2, 1)
-        place a PacMan at position (3, 1)
-        place a Ghost at position (4, 1)
+        place a wall at position (2, 1)
+        place a pacMan at position (3, 1)
+        place a ghost at position (4, 1)
 
-        dsl.map.entityAt(position(2, 1)) shouldBe Some(Wall)
+        dsl.map.entityAt(position(2, 1)) shouldBe Some(wall)
         dsl.map.entityAt(position(0, 1)).isEmpty shouldBe true
-        dsl.map.entityAt(position(3, 1)) shouldBe Some(PacMan)
-        dsl.map.entityAt(position(4, 1)) shouldBe Some(Ghost)
+        dsl.map.entityAt(position(3, 1)) shouldBe Some(pacMan)
+        dsl.map.entityAt(position(4, 1)) shouldBe Some(ghost)
