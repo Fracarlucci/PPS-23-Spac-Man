a1c3fd8f3971eef89e4573f8957a2f867bfc3b8d
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-11-09 20:42:58 +0100

    feat: add MovableEntity and its test

--- src/main/scala/model/GameEntity.scala
+++ src/main/scala/model/GameEntity.scala
@@ -1,9 +1,20 @@
 package model
 
-sealed trait GameEntity
+trait GameEntity:
+    def position: Position2D
 
-case object Wall extends GameEntity
-case object PacMan extends GameEntity
-case object Ghost extends GameEntity
+trait MovableEntity extends GameEntity:
 
+    def direction: Direction
 
+    def withPosAndDir(newPosition: Position2D, newDirection: Direction): MovableEntity
+
+    def move(newDirection: Direction): MovableEntity =
+        val newPosition = newDirection match
+            case Direction.Up    => Position2D(position.x, position.y - 1)
+            case Direction.Down  => Position2D(position.x, position.y + 1)
+            case Direction.Left  => Position2D(position.x - 1, position.y)
+            case Direction.Right => Position2D(position.x + 1, position.y)
+        withPosAndDir(newPosition, newDirection)
+
+case class Wall(position: Position2D) extends GameEntity
