cb77dc9382905587c6f42d1c5ea324a4014a474c
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-11-10 11:51:31 +0100

    feat: impl map methods for returning sets

--- src/test/scala/model/map/MapTest.scala
+++ src/test/scala/model/map/MapTest.scala
@@ -12,6 +12,7 @@ import model.SpacManBasic
 import model.GhostBasic
 import model.Direction
 import model.GameEntity
+import model.DotBasic
 
 class MapTest extends AnyFlatSpec with Matchers:
 
@@ -60,6 +61,55 @@ class MapTest extends AnyFlatSpec with Matchers:
         val wall = Wall(Position2D(-1, -1))
         val result = map.place(Position2D(-1, -1), wall)
         result.isLeft shouldBe true
+
+    it should "return the set of walls" in:
+        val dsl = MapDSL(map)
+        val wall1 = Wall(Position2D(5, 1))
+        val wall2 = Wall(Position2D(4, 1))
+        val wall3 = Wall(Position2D(3, 1))
+
+        import dsl.* 
+
+        place a wall1 at position (5, 1)
+        place a wall2 at position (4, 1)
+        place a wall3 at position (3, 1)
+
+        dsl.map.getWalls shouldBe Set(wall1, wall2, wall3) 
+
+    
+    it should "return the set of ghosts" in:
+        val dsl = MapDSL(map)
+        val ghost1 = GhostBasic(Position2D(5, 1), Direction.Right, 1.0, 1)
+        val ghost2 = GhostBasic(Position2D(4, 1), Direction.Right, 1.0, 1)
+        val ghost3 = GhostBasic(Position2D(3, 1), Direction.Right, 1.0, 1)
+
+        import dsl.* 
+
+        place a ghost1 at position (5, 1)
+        place a ghost2 at position (4, 1)
+        place a ghost3 at position (3, 1)
+
+        dsl.map.getGhosts shouldBe Set(ghost1, ghost2, ghost3) 
+
+
+    it should "return the set of dots" in:
+        val dsl = MapDSL(map)
+        val dot1 = DotBasic(Position2D(5, 1))
+        val dot2 = DotBasic(Position2D(4, 1))
+        val dot3 = DotBasic(Position2D(3, 1))
+
+        import dsl.* 
+
+        place a dot1 at position (5, 1)
+        place a dot2 at position (4, 1)
+        place a dot3 at position (3, 1)
+
+        dsl.map.getDots shouldBe Set(dot1, dot2, dot3) 
+    
+    it should "return an empty sets" in:
+        map.getWalls shouldBe Set.empty
+        map.getGhosts shouldBe Set.empty
+        map.getDots shouldBe Set.empty
 
     it should "return true calling canMove" in:
         val dsl = MapDSL(map)
