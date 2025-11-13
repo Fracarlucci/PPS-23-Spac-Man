c64a571ea5a04d49715172f8ba0d628ad4f11c77
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-11-13 15:23:50 +0100

    chore: add GhostBuilder

--- src/main/scala/model/GhostBasic.scala
+++ src/main/scala/model/GhostBasic.scala
@@ -1,10 +1,23 @@
 package model
 
-case class GhostBasic(val position: Position2D, val direction: Direction, val speed: Double, val id: Int) extends MovableEntity:
-   
-    override def withPosAndDir(newPosition: Position2D, newDirection: Direction): GhostBasic =
-        this.copy(position = newPosition, direction = newDirection)
+case class GhostBasic(
+    val position: Position2D,
+    val direction: Direction,
+    val speed: Double,
+    val id: Int
+) extends MovableEntity:
 
-    def nextMove(): MovableEntity =
-        val randomDir = Direction.values.toSeq(scala.util.Random.nextInt(Direction.values.size))
-        this.move(randomDir)
+  override def withPosAndDir(newPosition: Position2D, newDirection: Direction): GhostBasic =
+    this.copy(position = newPosition, direction = newDirection)
+
+  def nextMove(): MovableEntity =
+    val randomDir = Direction.values.toSeq(scala.util.Random.nextInt(Direction.values.size))
+    this.move(randomDir)
+
+object GhostBuilder:
+  var idCounter = 0
+  def apply(position: Position2D, direction: Direction, speed: Double): GhostBasic =
+    idCounter = idCounter + 1
+    GhostBasic(position, direction, speed, idCounter)
+
+  def reset(): Unit = idCounter = 0
--- src/test/scala/model/GhostTest.scala
+++ src/test/scala/model/GhostTest.scala
@@ -1,8 +1,9 @@
 package model
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class GhostTest extends AnyFlatSpec:
+class GhostTest extends AnyFlatSpec with Matchers:
     val startposition = Position2D(0, 0)
     val startDirection = Direction.Right
     val speed = 1.0
@@ -15,3 +16,20 @@ class GhostTest extends AnyFlatSpec:
     it should "change position" in:
         val newGhost = ghost.nextMove()
         assert(newGhost.position != startposition)
+
+    "GhostBuilder" should "create two ghost with different IDs" in:
+        val g1 = GhostBuilder(Position2D(0, 0), Direction.Up, 1.0)
+        val g2 = GhostBuilder(Position2D(1, 1), Direction.Down, 1.0)
+
+        g1.id shouldBe 1
+        g2.id shouldBe 2
+
+    it should "reset IDs" in:
+        val g1 = GhostBuilder(Position2D(0, 0), Direction.Up, 1.0)
+        val g2 = GhostBuilder(Position2D(1, 1), Direction.Down, 1.0)
+        GhostBuilder.reset()
+        val g3 = GhostBuilder(Position2D(1, 1), Direction.Down, 1.0)
+        
+        g1.id shouldBe 1
+        g2.id shouldBe 2
+        g3.id shouldBe 1
