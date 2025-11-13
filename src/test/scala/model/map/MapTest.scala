734adc8bebe917a0ead5fee78f52e0deaad44967
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-11-13 13:38:50 +0100

    chore: add placeAll in Map

--- src/test/scala/model/map/MapTest.scala
+++ src/test/scala/model/map/MapTest.scala
@@ -8,6 +8,7 @@ import model.Position2D
 import model.board
 import model.GameEntity
 import model.DotBasic
+import model.WallBuilder
 
 class MapTest extends AnyFlatSpec with Matchers:
 
@@ -53,6 +54,23 @@ class MapTest extends AnyFlatSpec with Matchers:
         val wall = Wall(Position2D(-1, -1))
         val result = map.place(Position2D(-1, -1), wall)
         result.isLeft shouldBe true
+    
+    it should "place all the entities in the map" in:
+        val walls = WallBuilder.createWalls(Position2D(0, 0), Position2D(0, 3))
+        val result = map.placeAll(walls)
+
+        result match
+            case Right(map)      => map.getWalls shouldBe walls
+            case Left(errMsg)    => fail(errMsg)
+    
+    it should "fail while placing all the entities in the map" in:
+        val walls = WallBuilder.createWalls(Position2D(0, -2), Position2D(0, 3))
+        val result = map.placeAll(walls)
+
+        result match
+            case Right(map)      => fail("The map should fail")
+            case Left(errMsg)    => errMsg shouldBe "Invalid position" + Position2D(0, -2)
+        
 
     it should "return the set of walls" in:
         val dsl = MapDSL(map)
