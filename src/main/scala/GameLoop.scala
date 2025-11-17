5ee4c3ccc979ef9dfb544851009d1925d9c5cff6
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-11-18 00:41:39 +0100

    feat!: main now uses GameLoop

--- src/main/scala/GameLoop.scala
+++ src/main/scala/GameLoop.scala
@@ -1,15 +1,4 @@
 import model.GameManager
-import java.util.Timer
-import model.MapDSL
-import model.board
-import model.genericWall
-import model.DotBasic
-import model.Position2D
-import model.GhostBasic
-import model.Direction
-import model.map.GameMap
-import model.SimpleGameManager
-import model.SpacManBasic
 
 enum GameState:
     case Running
@@ -32,7 +21,6 @@ object GameLoop:
         val now                = System.currentTimeMillis()
         state match
             case GameState.Running =>
-                // println("Start step ...")
                 if now - lastGhostMove >= ghost_delay then
                     gameManager.moveGhosts()
                     leatestGhostMove = now
@@ -52,33 +40,8 @@ object GameLoop:
         if gameManager.isWin() then
             println("WIN!")
             return GameState.Win
-        if gameManager.isGameOver() then
+        else if gameManager.isGameOver() then
             println("LOSE!")
             return GameState.GameOver
-        GameState.Running
-
-// Created only for demo purpose
-private def createMap(): GameMap =
-    val dsl    = MapDSL(board(10, 10))
-    val dot    = DotBasic(Position2D(0, 0))
-    val ghost1 = GhostBasic(Position2D(1, 1), Direction.Down, 1.0, 1)
-    val ghost2 = GhostBasic(Position2D(2, 2), Direction.Up, 1.0, 2)
-    import dsl.*
-
-    place a genericWall() from position(4, 4) to position(6, 6)
-    place a ghost1 at position(1, 1)
-    place a ghost2 at position(2, 2)
-    place a dot at position(0, 0)
-
-    println(dsl.map.getWalls)
-    dsl.map
-
-@main def main(): Unit =
-    val map         = createMap()
-    val spacman     = SpacManBasic(Position2D(9, 9), Direction.Down, 0)
-    val gameManager = SimpleGameManager(spacman, map)
-    val inputManager = new SimpleInputManager(gameManager)
-    
-    inputManager.startInputThread()
-
-    GameLoop.loop(gameManager, inputManager = inputManager)
+        else
+            GameState.Running
