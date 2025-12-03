d250340775f0fdbc3459fe1db9461ec4cf91fd76
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-12-03 11:00:39 +0100

    style: codes style

--- src/main/scala/model/Direction.scala
+++ src/main/scala/model/Direction.scala
@@ -2,9 +2,9 @@ package model
 
 sealed trait Direction
 object Direction:
-    case object Up extends Direction
-    case object Down extends Direction
-    case object Left extends Direction
+    case object Up    extends Direction
+    case object Down  extends Direction
+    case object Left  extends Direction
     case object Right extends Direction
 
     val values: Set[Direction] = Set(Up, Down, Left, Right)
