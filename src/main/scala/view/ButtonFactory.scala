dc803d8a7f76d45572fa39c5a4518abe0ef4b85c
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-12-14 13:14:25 +0100

    refactor: add consts for magic numbers

--- src/main/scala/view/ButtonFactory.scala
+++ src/main/scala/view/ButtonFactory.scala
@@ -4,6 +4,10 @@ import scala.swing.Button
 import scala.swing.Dimension
 import java.awt.Font
 
+val BUTTON_BIG_HORIZONTAL_SIZE = 300
+val BUTTON_NORMAL_HORIZONTAL_SIZE =100
+val BUTTON_VERTICAL_SIZE = 50
+
 enum ButtonSize:
     case Big
     case Normal
@@ -19,5 +23,5 @@ object ButtonFactory:
             font = fontType
 
     def dimension(size: ButtonSize): Dimension = size match
-        case ButtonSize.Big    => new Dimension(300, 50)
-        case ButtonSize.Normal => new Dimension(100, 50)
+        case ButtonSize.Big    => new Dimension(BUTTON_BIG_HORIZONTAL_SIZE, BUTTON_VERTICAL_SIZE)
+        case ButtonSize.Normal => new Dimension(BUTTON_NORMAL_HORIZONTAL_SIZE, BUTTON_VERTICAL_SIZE)
